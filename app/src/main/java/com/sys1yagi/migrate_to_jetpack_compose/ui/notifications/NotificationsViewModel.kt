bf8c09bca4bf700dfc257f2618ad84a461487a05
Toshihiro Yagi <sylc.yagi@gmail.com>
2021-01-16 14:15:40 +0900

    first commit.

---
+++ app/src/main/java/com/sys1yagi/migrate_to_jetpack_compose/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.sys1yagi.migrate_to_jetpack_compose.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
